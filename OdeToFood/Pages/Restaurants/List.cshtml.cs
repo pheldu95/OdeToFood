3f7da4eb6ac15768c7351a45c9239cd251948e2b
Duncan Phelps <phelps.duncan@gmail.com>
2020-05-06 14:26:32 -0500

    added search function to restaurant page. now onto the details page.

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -19,6 +19,12 @@ namespace OdeToFood.Pages.Restaurants
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
 
+        //BindProperty says that this property expects some information from the request we are sending
+        //this is model binding
+        //we have to add SupportsGet = true because the default BindProperty only works for a Post request
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
         //give page model a constructor
         public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
@@ -27,8 +33,11 @@ namespace OdeToFood.Pages.Restaurants
         }
         public void OnGet()
         {
+            //model binding. tell the ASP.NET core framework that you need an input that is named searchTerm.
+            //the framework will go out and look for the input named searchTerm
+            
             Message = config["Message"];
-            Restaurants = restaurantData.GetAll();
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
