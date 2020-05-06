a205a1ded6577c98b62bfcc5853b0e41d2df2832
Duncan Phelps <phelps.duncan@gmail.com>
2020-05-06 11:25:38 -0500

    have list of restaurants displaying. not coming from a db yet.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Core;
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Azada", Location = "Colorado", Cuisine = CuisineType.Mexican},
+                new Restaurant {Id = 3, Name = "Ganga", Location = "Yangshuo", Cuisine = CuisineType.Indian}
+
+
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
